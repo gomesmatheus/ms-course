f0921d31e161a037d48431ad0dd1545dbca1a369
Matheus Gomes <gom.matheus@hotmail.com>
2021-06-08 17:42:07 -0300

    Project hr-worker created

---
+++ hr-worker/src/test/java/gomesmatheus/com/github/hrworker/HrWorkerApplicationTests.java
@@ -0,0 +1,13 @@
+package gomesmatheus.com.github.hrworker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrWorkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
