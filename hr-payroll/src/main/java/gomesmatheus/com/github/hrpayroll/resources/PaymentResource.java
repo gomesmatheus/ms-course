f81fd2a4417691b89bac6bce482d295dcdf17eff
Matheus Gomes <gom.matheus@hotmail.com>
2021-06-10 00:20:59 -0300

    RestTemplate

--- hr-payroll/src/main/java/gomesmatheus/com/github/hrpayroll/resources/PaymentResource.java
+++ hr-payroll/src/main/java/gomesmatheus/com/github/hrpayroll/resources/PaymentResource.java
@@ -1,14 +1,13 @@
 package gomesmatheus.com.github.hrpayroll.resources;
 
+import gomesmatheus.com.github.hrpayroll.entities.Payment;
+import gomesmatheus.com.github.hrpayroll.services.PaymentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import gomesmatheus.com.github.hrpayroll.entities.Payment;
-import gomesmatheus.com.github.hrpayroll.services.PaymentService;
 
 @RestController
 @RequestMapping(value = "/payments")
