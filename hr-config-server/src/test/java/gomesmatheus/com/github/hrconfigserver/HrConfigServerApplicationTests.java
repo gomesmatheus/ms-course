8813ecc2033a06f1c732efd7bac92e52a75ae01f
Matheus Gomes <vsgmatheus@gmail.com>
2021-06-14 17:05:20 -0300

    Project hr-config-server created

---
+++ hr-config-server/src/test/java/gomesmatheus/com/github/hrconfigserver/HrConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package gomesmatheus.com.github.hrconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
